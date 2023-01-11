8968ba6875b30a8fc41f967c1e4f0f8f900393d8
Frostork <91312714+Frostork@users.noreply.github.com>
2023-01-11 16:43:02 +0100

    PlayerController

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Bomb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody _rigidbody;
+    private Vector3 _lastVelocity;
+
+    private float _speed;
+    private Vector3 _direction;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        StartCoroutine(Explosed());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _lastVelocity = _rigidbody.velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(new Vector3(1000,0,0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        _speed = _lastVelocity.magnitude;
+        _direction = Vector3.Reflect(_lastVelocity.normalized, col.contacts[0].normal);
+
+        _rigidbody.velocity = _direction * Mathf.Max(_speed, 0f);
+    }
+
+    IEnumerator Explosed()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
     }
 }
