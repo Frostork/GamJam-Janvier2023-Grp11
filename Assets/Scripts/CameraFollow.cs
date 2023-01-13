30fc74fbcdb76c9c0f959314d5cb9cf057800f96
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2023-01-13 01:32:59 +0100

    CarPlayer

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 _offSet;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _translateSpeed;
+    [SerializeField] private float _rotationSpeed;
+
+    private void FixedUpdate()
+    {
+        HandleTranslation();
+        HandleRotation();
+    }
+
+    private void HandleTranslation()
+    {
+        var targetPosition = _target.TransformPoint(_offSet);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _translateSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        var direction = _target.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,26 +1,36 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 50f;
-    [SerializeField] private float _maxSpeed = 15f;
-    [SerializeField] private float _drag = 0.98f;
-    [SerializeField] private float _steerAngle = 20f;
+    private float moveInput;
+    private float turnInput;
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+    
+    public Rigidbody SphereRigidbody;
 
-    private Vector3 MoveForce;
+    private void Start()
+    {
+        SphereRigidbody.transform.parent = null;
+    }
 
     private void Update()
     {
-        MoveForce += transform.forward * (_moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
-        transform.position += MoveForce * Time.deltaTime;
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+        
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+        
+        transform.position = SphereRigidbody.transform.position;
 
-        float steerInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * (steerInput * MoveForce.magnitude * _steerAngle * Time.deltaTime));
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
+    }
 
-        MoveForce *= _drag;
-        MoveForce = Vector3.ClampMagnitude(MoveForce, _maxSpeed);
+    private void FixedUpdate()
+    {
+        SphereRigidbody.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
     }
 }
